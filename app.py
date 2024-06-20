67343824f6e2d6ee9683b3b227604df1e2d1fb09
Sergio Ruiz <59260822+sruizs-dev@users.noreply.github.com>
2024-06-20 14:28:54 +0000

    Codigo de juego

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from random import choice
+
+
+option = input("Debes escribir una de las siguientes opciones: rock, paper o scissors. Presiona Enter para continuar...\n")
+
+optionMachine = choice(["rock", "paper", "scissors"])
+
+match(option.lower()):
+    case "rock":
+        if optionMachine == "rock":
+            print("Empate")
+        elif optionMachine == "paper":
+            print("Perdiste")
+        else:  
+            print("Ganaste")
+    case "paper":
+        if optionMachine == "rock":
+            print("Ganaste")
+        elif optionMachine == "paper":
+            print("Empate")
+        else:  
+            print("Perdiste")
+    case "scissors":
+        if optionMachine == "rock":
+            print("Perdiste")
+        elif optionMachine == "paper":
+            print("Ganaste")
+        else:  
+            print("Empate")
+    case _:
+        print("Opción no válida")
